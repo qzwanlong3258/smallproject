b76a46e01247a74d4487030522a71ba517e37aed
wanglong <739259613@qq.com>
2020-03-18 22:40:23 +0800

    艾衣秀h5

---
+++ aiyixiu/config/image.js
@@ -0,0 +1,11 @@
+
+
+module.exports={
+	/** 授权 */
+	ANDROID: `https://s1.ax1x.com/2020/03/18/80Bfuq.png`,
+	IOS: `https://s1.ax1x.com/2020/03/18/80BzVK.png`,
+	LOGO:`https://s1.ax1x.com/2020/03/18/800sp9.png`
+	
+	
+	
+}
---
+++ aiyixiu/pages/index/index.vue
@@ -0,0 +1,96 @@
+<template>
+	<view class="body">
+		<image class="logo" mode="widthFix" :src="img[0]"></image>
+		<view class="btn-page">
+			<view class="btnone"  @click="submitTest">
+				
+				
+			</view>
+			<image v-if="shoeOne" class="codeOne" :src="img[1]" mode="widthFix"></image>
+			<view class="btntwo"  @click="submitApply">
+			</view>
+			<image class="codeTwo" v-if="shoeTwo" :src="img[2]" mode="widthFix"></image>
+		</view>
+	</view>
+</template>
+
+<script>
+	import { ANDROID, IOS, LOGO } from '@/config/image.js';
+	var _self;
+	export default {
+		data() {
+			return {
+				shoeOne:false,
+				shoeTwo:false,
+				img:[LOGO,ANDROID,IOS]
+			}
+		},
+		onLoad() {
+			_self=this
+
+		},
+		methods: {
+			submitTest(){
+				_self.shoeOne=!this.shoeOne
+				_self.shoeTwo = false
+			},
+			submitApply(){
+				_self.shoeTwo=!this.shoeTwo
+				_self.shoeOne = false
+			}
+
+		}
+	}
+</script>
+
+<style>
+	.body {
+		width: 100%;
+	}
+
+	.logo {	
+		width: 100%;
+		display: block;
+		position: absolute;
+	}
+	.btn-page{
+		width: 100%;
+		height: 100%;
+		
+		position: absolute;
+	}
+	.btnone{
+		height: 20rpx;
+		line-height: 80rpx;
+		width: 90rpx;
+		
+		border-radius: 45rpx;
+		position: absolute;
+		right: 140rpx;
+		top:29rpx;
+	}
+	.btntwo{
+		height: 20rpx;
+		line-height: 83rpx;
+		width: 65rpx;
+		
+		border-radius: 45rpx;
+		position: absolute;
+		right: 55rpx;
+		top:29rpx;
+	}
+	.codeOne{
+		width: 100rpx;
+		position: absolute;
+		right: 134rpx;
+		top:45rpx;
+	}
+	.codeTwo{
+		width: 100rpx;
+		position: absolute;
+		right: 36rpx;
+		top:45rpx;
+	}
+
+	
+</style>
